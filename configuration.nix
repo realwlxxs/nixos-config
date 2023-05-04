6a95013785ce2e0b7501208d6bd2680cbe469bfc
Zhengyang Lu <realwlxxs@163.com>
2023-05-04 13:38:19 +0800

    feat(configuration.nix): add python pkgs for my homework

--- configuration.nix
+++ configuration.nix
@@ -77,8 +77,17 @@
     htop
     sqlite
     texlive.combined.scheme-full
-    (python3.withPackages
-      (ps: with ps; [ pandas black pyflakes isort tkinter ]))
+    (python3.withPackages (ps:
+      with ps; [
+        pandas
+        black
+        pyflakes
+        isort
+        tkinter
+        matplotlib
+        jieba
+        wordcloud
+      ]))
     nodejs
     xclip
     godot
