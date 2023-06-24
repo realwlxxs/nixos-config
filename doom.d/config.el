a76e7b372ec9a818034172f08ce715c310d9753a
Zhengyang Lu <realwlxxs@163.com>
2023-06-24 21:33:07 +0800

    feat: change emacs font

--- doom.d/config.el
+++ doom.d/config.el
@@ -1,1 +1,6 @@
 (setq doom-theme 'parchment)
+
+(setq doom-font (font-spec :family "Sarasa Mono Slab SC" :size 14 :weight 'regular)
+      doom-variable-pitch-font (font-spec :family "Sarasa Mono Slab SC")
+      doom-unicode-font (font-spec :family "Sarasa Mono Slab SC" :size 14)
+      doom-big-font (font-spec :family "Sarasa Mono Slab SC" :size 20))
