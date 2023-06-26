8959f56e078bf8c4657a48f26f3897d87e2d1033
Zhengyang Lu <realwlxxs@163.com>
2023-06-26 21:16:56 +0800

    feat: change emacs theme

--- doom.d/config.el
+++ doom.d/config.el
@@ -1,3 +1,5 @@
+(setq doom-theme 'modus-operandi)
+
 (setq doom-font (font-spec :family "Sarasa Mono Slab SC" :size 14 :weight 'regular)
       doom-variable-pitch-font (font-spec :family "Sarasa Mono Slab SC")
       doom-unicode-font (font-spec :family "Sarasa Mono Slab SC" :size 14)
