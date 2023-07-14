1bbb3ae1bf411109432c8a4e177357b421d15256
Zhengyang Lu <realwlxxs@163.com>
2023-07-14 10:51:23 +0800

    feat: enable auto save file for emacs

--- doom.d/config.el
+++ doom.d/config.el
@@ -4,3 +4,5 @@
       doom-variable-pitch-font (font-spec :family "Sarasa Mono Slab SC")
       doom-unicode-font (font-spec :family "Sarasa Mono Slab SC" :size 14)
       doom-big-font (font-spec :family "Sarasa Mono Slab SC" :size 20))
+
+(setq auto-save-default t)
