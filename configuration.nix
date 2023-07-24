8a7cb75408cdac0d6a5acc1b7bb478a73a1c7a62
Zhengyang Lu <realwlxxs@163.com>
2023-07-24 14:50:07 +0800

    style: format configs

--- configuration.nix
+++ configuration.nix
@@ -174,11 +174,11 @@
     authentication = pkgs.lib.mkOverride 10 ''
       #type database DBuser origin-address auth-method
       # unix
-      local all      all                    trust
+      local all      all                   trust
       # ipv4
-      host  all      all     127.0.0.1/32   trust
+      host  all      all    127.0.0.1/32   trust
       # ipv6
-      host all       all     ::1/128        trust
+      host  all      all    ::1/128        trust
     '';
     initialScript = pkgs.writeText "backend-initScript" ''
       CREATE ROLE nixcloud WITH LOGIN PASSWORD 'nixcloud' CREATEDB;
