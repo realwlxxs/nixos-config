a47b856adb7c59355e3e048b874734eb53bea01b
Zhengyang Lu <realwlxxs@163.com>
2023-07-24 14:32:16 +0800

    feat(pgsql): enable unix socket access

--- configuration.nix
+++ configuration.nix
@@ -172,8 +172,9 @@
     ensureDatabases = [ "mydatabase" ];
     enableTCPIP = true;
     authentication = pkgs.lib.mkOverride 10 ''
-      #...
       #type database DBuser origin-address auth-method
+      # unix
+      local all      all                    trust
       # ipv4
       host  all      all     127.0.0.1/32   trust
       # ipv6
