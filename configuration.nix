874cab1b69381b65c9e459157bb1d84676aaa1fb
Zhengyang Lu <realwlxxs@163.com>
2023-08-01 11:58:57 +0800

    feat: add caps2esc

--- configuration.nix
+++ configuration.nix
@@ -182,6 +182,17 @@
 
   networking.firewall.enable = false;
 
+  services.interception-tools = {
+    enable = true;
+    plugins = [ pkgs.interception-tools-plugins.caps2esc ];
+    udevmonConfig = ''
+      - JOB: "${pkgs.interception-tools}/bin/intercept -g $DEVNODE | ${pkgs.interception-tools-plugins.caps2esc}/bin/caps2esc | ${pkgs.interception-tools}/bin/uinput -d $DEVNODE"
+        DEVICE:
+          EVENTS:
+            EV_KEY: [KEY_CAPSLOCK, KEY_ESC]
+    '';
+  };
+
   nix.gc = {
     automatic = true;
     options = "--delete-older-than 5d";
